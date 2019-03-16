32883eb06fa0b2b28551f26691c6e6fb4069eaba
Fred Buchanan <oscillator.b@gmail.com>
2019-03-16 14:38:17 -0400

    Kind of works

--- static/index.js
+++ static/index.js
@@ -1,4 +1,25 @@
 
-var app = new Vue({
-  el:'#app'
+Vue.component('emissions-slider', {
+  props: ['name'],
+  data: function () {
+    return {
+      'value':100
+    }
+  },
+  template: `
+  <v-container fluid grid-list-lg>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-slider ref="slider" v-bind:label="name" v-model="value"></v-slider>
+        <div class="slider-value">{{value}}</div>
+      </v-flex>
+    </v-layout>
+  </v-container>`,
 })
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    gtc02: 100
+  }
+});
