342bf610c8cadd8f3c65fdc353919e0d5e6dd064
Fred Buchanan <oscillator.b@gmail.com>
2019-03-16 18:44:54 -0400

    CSS changes

--- static/index.js
+++ static/index.js
@@ -3,7 +3,7 @@ Vue.component('emissions-slider', {
   props: ['name', 'value', 'scale'],
   data: function () {
     return {
-      'slider_value': this.value
+      'slider_value': 100
     }
   },
   methods: {
@@ -13,21 +13,52 @@ Vue.component('emissions-slider', {
     }
   },
   template: `
-  <v-container fluid grid-list-lg style="padding: 0px !important">
-    <v-layout row wrap>
-      <v-flex xs12>
-        <v-slider ref="slider" v-bind:label="name" v-model="slider_value" @input="update" min="0" max="200"></v-slider>
-        <div class="slider-value">{{slider_value}}%</div>
-      </v-flex>
-    </v-layout>
-  </v-container>`,
+        <div>
+          <p>
+            <h3>{{name}}</h3>
+            <div class="slidercontainer">
+              <input class="slider" type="range" ref="slider" v-model="slider_value" @input="update" min="0" max="200"></input>
+            </div>
+          </p>
+          <div class="slider-value">{{slider_value}}%</div>
+        </div>`,
 })
+
+var options = {
+  title: {
+    text: 'Global Average Temperature',
+    color: "#fffff",
+    x: -20 //center
+  },
+  xAxis: {
+    crosshair: true
+  },
+  yAxis: {
+    title: {
+      text: 'Temperature (°C)'
+    },
+    plotLines: [{
+      value: 0,
+      width: 1,
+      color: '#808080'
+    }]
+  },
+  tooltip: {
+    valueSuffix: '°C'
+  },
+  series: [{
+    name: 'Global Mean Temperature',
+    data: [],
+    pointStart: 1765
+  }]
+}
 
 var app = new Vue({
   el: '#app',
   data: {
     sliders: [],
-    simulated: false
+    simulated: false,
+    chartOptions: options
   },
   beforeMount() {
     let self = this;
@@ -38,15 +69,25 @@ var app = new Vue({
         let sliders = json.sliders;
         console.log(sliders);
         for (let slider of sliders) {
-          slider['value'] = 100;
+          slider['value'] = slider.scale;
           self.sliders.push(slider)
         }
       })
   },
   methods: {
     simulate() {
-      this.simulated = true;
-      console.log("SIMULATE")
+      let self = this;
+      self.simulated = false;
+      var totalCarbon = this.sliders.map(s => s.value).reduce((a,b)=>a+b);
+      fetch('/api/simulate?fossil_gtc_carbon=' + totalCarbon).then(function(resp){
+        return resp.json();
+      }).then(function(body) {
+        console.log(body);
+        self.chartOptions.series[0].data = body.temperatures;
+        //self.chartOptions.series[0].pointStart = body.start_year;
+        self.simulated = true;
+        
+      })
     }
   }
 });
